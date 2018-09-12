a3f0c285f0296eeca68569ae557d2e38c4a5ed1b
CommuterCoin <commutercoin@gmail.com>
2018-09-12 19:12:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+Socialcoin integration/staging tree
+================================
+
+http://www.socialcoin.org
+
+Copyright (c) 2009-2014 Bitcoin Developers
+Copyright (c) 2017 SocialCoinNetwork Developers
+
+What is Socialcoin?
+----------------
+
+Socialcoin is a lite version of Bitcoin using scrypt as a proof-of-work algorithm.
+ - 4 minute block targets
+ - subsidy halves in 495k blocks (~2 years)
+ - ~75 million total coins
+
+The rest is the same as Bitcoin.
+ - 75 coins per block
+ - 2016 blocks to retarget difficulty
+
+For more information, as well as an immediately useable, binary version of
+the Socialcoin client sofware, see http://www.social-coin.co.uk
+
+SocialCoin is a crypto currency (in the same vein as Bitcoin and Ethereum, two of the well known brands in this space) specifically aimed at the Social Media industry. What does that mean? Read on, below.
+
+Consider how rewarding it would be to be to be paid for your reviews, views, likes and content online. That is exactly what SocialCoin, and The Social Coin Network, aims to achieve. 
+SocialCoin is the original and strongest Social Media currency and is finally bringing cryptocurrency to the masses.
+
+License
+-------
+
+Socialcoin is released under the terms of the MIT license. See `COPYING` for more
+information or see http://opensource.org/licenses/MIT.
+
+Development process
+-------------------
+
+Developers work in their own trees, then submit pull requests when they think
+their feature or bug fix is ready.
+
+If it is a simple/trivial/non-controversial change, then one of the Socialcoin
+development team members simply pulls it.
+
+If it is a *more complicated or potentially controversial* change, then the patch
+submitter will be asked to start a discussion with the devs and community.
+
+The patch will be accepted if there is broad consensus that it is a good thing.
+Developers should expect to rework and resubmit patches if the code doesn't
+match the project's coding conventions (see `doc/coding.txt`) or are
+controversial.
+
+The `master` branch is regularly built and tested, but is not guaranteed to be
+completely stable. [Tags](https://github.com/socialcoin-project/socialcoin/tags) are created
+regularly to indicate new official, stable release versions of Socialcoin.
+
+Testing
+-------
+
+Testing and code review is the bottleneck for development; we get more pull
+requests than we can review and test. Please be patient and help out, and
+remember this is a security-critical project where any mistake might cost people
+lots of money.
+
+### Automated Testing
+
+Developers are strongly encouraged to write unit tests for new code, and to
+submit new unit tests for old code.
+
+Unit tests for the core code are in `src/test/`. To compile and run them:
+
+    cd src; make -f makefile.unix test
+
+Unit tests for the GUI code are in `src/qt/test/`. To compile and run them:
+
+    qmake BITCOIN_QT_TEST=1 -o Makefile.test bitcoin-qt.pro
+    make -f Makefile.test
+    ./socialcoin-qt_test
+
---
+++ src/checkpoints.cpp
@@ -0,0 +1,135 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "main.h"
+#include "uint256.h"
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    // How many times we expect transactions after the last checkpoint to
+    // be slower. This number is a compromise, as it can't be accurate for
+    // every system. When reindexing from a fast disk with a slow CPU, it
+    // can be up to 20, while when downloading from a slow network with a
+    // fast multicore CPU, it won't be much higher than 1.
+    static const double fSigcheckVerificationFactor = 5.0;
+
+    struct CCheckpointData {
+        const MapCheckpoints *mapCheckpoints;
+        int64 nTimeLastCheckpoint;
+        int64 nTransactionsLastCheckpoint;
+        double fTransactionsPerDay;
+    };
+
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    static MapCheckpoints mapCheckpoints =
+        boost::assign::map_list_of
+        (  0, uint256("0x716b50a3d5c5e9c5cd4fdbdf68c618712e3595cc324053ffd617082103cf5e6b"))
+        ( 25, uint256("0x55d4f30ea79483d1c696a3a0f7983c45d5aa225469a785461ecccca37ce1df04"))
+        ( 4965, uint256("067a58984c7d8c6dc7d00fcbd4490e65c185c281bf766ebf449c83380e25cd24c"))
+        ( 4966, uint256("0xa28a62d4f164af0555d95058b4aedb3d579fdfe9856769c7e035a0985c31703c"))
+        ( 4969, uint256("0xcb5f143094c7e5d6c9cb50ce2edeb8eb161a33e90082a1a3897ccdbaeb3e2195"))
+        ( 5008, uint256("0x6ccf2108e3123a3c9537a6ef5e072b148fbd971fc74081e5680900ef30ed8664"))
+        ( 5037, uint256("0x36ef16b66ed3dd651e8050027d581276e182b157269faf181fa8115123b82007"))
+        ( 5111, uint256("0xb0408098699944588de28cb4f8a5c7470dc4d7ac7143adafbe724aa18f7ec3e8"))
+        ;
+    static const CCheckpointData data = {
+        &mapCheckpoints,
+    };
+
+    static MapCheckpoints mapCheckpointsTestnet =
+        boost::assign::map_list_of
+        (   0, uint256("0x"))
+        ;
+    static const CCheckpointData dataTestnet = {
+        &mapCheckpointsTestnet,
+    };
+
+    const CCheckpointData &Checkpoints() {
+        if (fTestNet)
+            return dataTestnet;
+        else
+            return data;
+    }
+
+    bool CheckBlock(int nHeight, const uint256& hash)
+    {
+        if (!GetBoolArg("-checkpoints", true))
+            return true;
+
+        const MapCheckpoints& checkpoints = *Checkpoints().mapCheckpoints;
+
+        MapCheckpoints::const_iterator i = checkpoints.find(nHeight);
+        if (i == checkpoints.end()) return true;
+        return hash == i->second;
+    }
+
+    // Guess how far we are in the verification process at the given block index
+    double GuessVerificationProgress(CBlockIndex *pindex) {
+        if (pindex==NULL)
+            return 0.0;
+
+        int64 nNow = time(NULL);
+
+        double fWorkBefore = 0.0; // Amount of work done before pindex
+        double fWorkAfter = 0.0;  // Amount of work left after pindex (estimated)
+        // Work is defined as: 1.0 per transaction before the last checkoint, and
+        // fSigcheckVerificationFactor per transaction after.
+
+        const CCheckpointData &data = Checkpoints();
+
+        if (pindex->nChainTx <= data.nTransactionsLastCheckpoint) {
+            double nCheapBefore = pindex->nChainTx;
+            double nCheapAfter = data.nTransactionsLastCheckpoint - pindex->nChainTx;
+            double nExpensiveAfter = (nNow - data.nTimeLastCheckpoint)/86400.0*data.fTransactionsPerDay;
+            fWorkBefore = nCheapBefore;
+            fWorkAfter = nCheapAfter + nExpensiveAfter*fSigcheckVerificationFactor;
+        } else {
+            double nCheapBefore = data.nTransactionsLastCheckpoint;
+            double nExpensiveBefore = pindex->nChainTx - data.nTransactionsLastCheckpoint;
+            double nExpensiveAfter = (nNow - pindex->nTime)/86400.0*data.fTransactionsPerDay;
+            fWorkBefore = nCheapBefore + nExpensiveBefore*fSigcheckVerificationFactor;
+            fWorkAfter = nExpensiveAfter*fSigcheckVerificationFactor;
+        }
+
+        return fWorkBefore / (fWorkBefore + fWorkAfter);
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        if (!GetBoolArg("-checkpoints", true))
+            return 0;
+
+        const MapCheckpoints& checkpoints = *Checkpoints().mapCheckpoints;
+
+        return checkpoints.rbegin()->first;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        if (!GetBoolArg("-checkpoints", true))
+            return NULL;
+
+        const MapCheckpoints& checkpoints = *Checkpoints().mapCheckpoints;
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, checkpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+        }
+        return NULL;
+    }
+}
